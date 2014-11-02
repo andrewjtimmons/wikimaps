7f8eff22d72bb7b606b8e01882438cdd593267cc
andy timmons <andrewjtimmons@gmail.com>
2014-11-02 12:04:48 -0500

    refactor JS functions for simplicity

--- javascript.js
+++ javascript.js
@@ -100,7 +100,7 @@ $(document).ready(function () {
 
 					google.maps.event.addListener(marker, 'click', function() {
 						if (currentInfoWindow) currentInfoWindow.close();
-						infowindow.maxWidth=20;
+						infowindow.maxWidth=window.innerWidth*.9;
 						infowindow.open(map,marker);
 						currentInfoWindow = infowindow;
 					});
